077f8e3b64a13c7fa346249d81bf8e9af768fa74
HarrisonOwl <hyzitbios@hotmail.com>
2023-05-04 22:08:41 +0900

    wip ref #7 modify gpt.go

--- infrastructure/driver/gpt.go
+++ infrastructure/driver/gpt.go
@@ -2,33 +2,38 @@ package driver
 
 import (
 	"fmt"
+	"math"
+	"os"
+	"time"
+
 	"github.com/sashabaranov/go-openai"
 	"golang.org/x/net/context"
-	"math"
-	"time"
 )
 
 const retryLimit = 5
+
+var (
+	openAIKey = os.Getenv("OPENAI_KEY")
+)
 
 type GptDriver struct {
 	*openai.Client
 }
 
 func NewGptDriver() *GptDriver {
-
 	return &GptDriver{
-		openai.NewClient("your token"),
+		openai.NewClient(openAIKey),
 	}
 }
 
 func (d *GptDriver) RequestMessage(input string) (openai.ChatCompletionResponse, error) {
 	// バックオフリトライ
-	retryCnt := 0.0
+	retryCnt := 0
 	for {
 		resp, err := d.CreateChatCompletion(
 			context.Background(),
 			openai.ChatCompletionRequest{
-				Model: openai.GPT3Dot5Turbo,
+				Model: openai.GPT3Dot5Turbo0301,
 				Messages: []openai.ChatCompletionMessage{
 					{
 						Role:    openai.ChatMessageRoleSystem,
@@ -45,9 +50,10 @@ func (d *GptDriver) RequestMessage(input string) (openai.ChatCompletionResponse,
 			// error チェック
 			fmt.Println(err)
 		} else {
+			fmt.Println(resp.Choices[0].Message.Content)
 			return resp, nil
 		}
-		time.Sleep(time.Duration(math.Pow(2, retryCnt)) * time.Second)
+		time.Sleep(time.Duration(math.Pow(2, float64(retryCnt))) * time.Second)
 		retryCnt++
 		fmt.Println("retrying...")
 	}

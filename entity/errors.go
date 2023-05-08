6aeaf0063790bbf4edb6be94181ba24e23cd2669
gari8 <hagari8810@icloud.com>
2023-05-08 11:20:04 +0900

    ref #19 error replace en-message to ja-message

--- entity/errors.go
+++ entity/errors.go
@@ -1,5 +1,10 @@
 package entity
 
+import (
+	"log"
+	"net/http"
+)
+
 type Error struct {
 	Code    int
 	Message string
@@ -14,8 +19,25 @@ func (e Error) Error() string {
 }
 
 func NewError(code int, err error) *Error {
+	log.Println(err)
+	message, ok := messages[code]
+	if !ok {
+		return &Error{
+			Code:    code,
+			Message: messages[http.StatusInternalServerError],
+		}
+	}
 	return &Error{
 		Code:    code,
-		Message: err.Error(),
+		Message: message,
 	}
+}
+
+var messages = map[int]string{
+	http.StatusBadRequest:          "リクエストの形式が正しくありません。",
+	http.StatusUnauthorized:        "認証に失敗しました。ログイン状態を確認してください。",
+	http.StatusForbidden:           "管理者権限がありません。",
+	http.StatusNotFound:            "お探しのリソースは存在しません。",
+	http.StatusConflict:            "リクエストされたコンテンツは既に存在しています。リソースを確認して再度リクエストしてください。",
+	http.StatusInternalServerError: "予期せぬエラーが発生しました。",
 }
